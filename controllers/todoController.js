00847c790cc5c1f13660c2c0fd2256e737236529
Bhongo <sinxo.bhongolethu@gmail.com>
2022-06-04 23:03:38 +0200

    2 files modified

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,10 @@
+let data = [ { item: 'Feed the dog' }, { item: 'Learn Node JS'}, { item: 'Go Shopping'}];
+
 module.exports = function(app){
 
     //setting up routes
     app.get('/todo', function(req, res) {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
     app.post('/todo', function(req, res) {
