dfdbe6b5becb6636969c0035d902510724f50c50
Bhongo <sinxo.bhongolethu@gmail.com>
2022-06-04 22:05:04 +0200

    project set up done

---
+++ controllers/todoController.js
@@ -0,0 +1,15 @@
+module.exports = function(app){
+
+    //setting up routes
+    app.get('/todo', function(req, res) {
+
+    });
+
+    app.post('/todo', function(req, res) {
+      
+    });
+
+    app.delete('/todo', function(req, res) {  // http request
+      
+    });
+};
